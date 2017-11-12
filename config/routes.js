428c34843caacc5f1dd8f3b4042ab9ff3606a2c8
plummoore <plummoore@gmail.com>
2017-11-12 13:17:18 +0000

    routes tested OK

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,17 @@
 const express = require('express');
 const router  = express.Router();
 
+const users = require('../controllers/users');
+
+//USERS
+router.route('/users')
+  .get(users.index)
+  .post(users.create);
+
+router.route('/users/:id')
+  .get(users.show)
+  .put(users.update)
+  .delete(users.delete);
+
+
 module.exports = router;
