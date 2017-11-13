4c5ca9d31c15e82ff855c3965681c8393e7447c2
plummoore <plummoore@gmail.com>
2017-11-13 18:38:26 +0000

    post comment working

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,15 +1,12 @@
 const User = require('../models/user');
 const Group = require('../models/group');
 
-
 function commentCreate(req, res) {
-  console.log('COMMENT WORKING!!');
   Group
     .findById(req.params.id)
     .exec()
     .then(group => {
-      if(!group) return res.status(404).json({ message: 'Group not found.' });
-      console.log(req.user);
+      if (!group) return res.status(404).json({ message: 'Group not found.' });
       req.body.createdBy = req.user.userId;
       group.comments.push(req.body); // create an embedded record
       group.save();
--- src/js/controllers/groups/groups-show.controller.js
+++ src/js/controllers/groups/groups-show.controller.js
@@ -2,9 +2,18 @@ angular
   .module('groupProject')
   .controller('groupsShowCtrl', groupsShowCtrl);
 
-groupsShowCtrl.$inject = ['Group', '$stateParams'];
-function groupsShowCtrl(Group, $stateParams) {
+groupsShowCtrl.$inject = ['Group', '$stateParams', '$rootScope'];
+function groupsShowCtrl(Group, $stateParams, $rootScope) {
   const vm = this;
 
   vm.group = Group.get({ id: $stateParams.id });
+
+  vm.commentCreate = () => {
+    Group
+      .comment({ id: $stateParams.id }, vm.comment)
+      .$promise
+      .then(res => {
+        vm.group = res;
+      });
+  };
 }
