04da7d5a47c16aa015df81c98bf10cb16a892e7f
mattyates52 <matt.yates52@gmail.com>
2017-11-13 16:23:52 +0000

    api complete (sort of)

--- src/js/directives/googleMap.js
+++ src/js/directives/googleMap.js
@@ -1,12 +1,11 @@
+/* global google:ignore */
+
 angular
   .module('groupProject')
   .directive('googleMap', googleMap);
 
-// const map = document.getElementsByClassName('map');
-// let map = null;
-
-
 googleMap.$inject = ['$window', '$http'];
+
 function googleMap($window, $http) {
   return {
     restrict: 'E',
@@ -16,19 +15,51 @@ function googleMap($window, $http) {
       center: '='
     },
     link(scope, element) {
-      $http.get('http://localhost:7000/api/events')
-        .then(function successCallback(response){
-          const json = JSON.parse(response.data);
-          console.log(json);
-        });
-        
+
       const map = new $window.google.maps.Map(element[0], {
         zoom: 14,
         center: scope.center
       });
-      //loop trough the array an generate one marker
+
+
+      $http.get('http://localhost:7000/api/events')
+        .then(function successCallback(response){
+          const data = JSON.parse(response.data);
+          console.log(data);
+          data.events.event.forEach((location) => {
+            addMarker(location);
+          });
+        });
+
+      function addMarker(location) {
+        const latLng = { lat: parseFloat(location.latitude), lng: parseFloat(location.longitude) };
+
+        const marker = new $window.google.maps.Marker({
+          position: latLng,
+          map: map
+          // add in marker icon here
+        });
+        marker.addListener('click', () => {
+          createInfoWindow(marker, location);
+        });
+      }
+      function createInfoWindow(marker, location) {
+        const infowindow = new google.maps.InfoWindow({
+          content: `
+          <div class="infowindow">
+            <h3>${location.title}</h3>
+            <h3>${location.start_time}</h3>
+            <h3>${location.venue_address}</h3>
+          </div>
+          `
+          // adress field will not render
+        });
+        infowindow.open(map, marker);
+      }
+
     }
   };
 }
+
 
 // url: 'http://api.eventful.com/rest/events/search?app_key=4NdXJf3wjWsTGctn&keywords=books&location=San+Diego&date=Future'
