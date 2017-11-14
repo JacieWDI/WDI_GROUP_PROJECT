59d24cf1e7c4a1e7e2d24d5ea654ffe4e2a36d15
“Julie-S-Bernal“ <Julies.bernal@gmail.coml>
2017-11-14 11:17:30 +0000

    users can now edit their informations

--- src/js/controllers/users/users-edit.controller.js
+++ src/js/controllers/users/users-edit.controller.js
@@ -6,17 +6,33 @@ usersEditCtrl.$inject = ['User', '$stateParams', '$state'];
 function usersEditCtrl(User, $stateParams, $state) {
   const vm = this;
   vm.user = User.get($stateParams);
+  // vm.submit = submit;
+
+//   function submit(user){
+//     console.log('working1');
+//     User
+//     .save(user)
+//     // console.log('working2')
+//     .$promise
+//     // console.log('working4')
+//     .then(()=>{
+//       $state.go('home');
+//       // $state.go('usersShow', { id: user._id });
+//       // console.log('working4');
+//     });
+//   }
+//
+// }
+
 
   vm.submitForm = update;
-console.log('working1');
-  function update(user) {
-    console.log('working2');
+  console.log('working1');
+  function update() {
     User
-      .update({ id: user._id }, user)
+      .update({ id: $stateParams.id }, vm.user)
       .$promise
       .then(user => {
-        $state.go('usersShow', { id: user._id });
+        $state.go('usersShow', { id: $stateParams.id });
       });
-      console.log('working3');
-  }
+    }
 }
