43a97c561fc6f1c6cb98e39cb70b113aabbaf350
plummoore <plummoore@gmail.com>
2017-11-14 10:52:15 +0000

    typo fixed

--- src/js/controllers/users/users-edit.controller.js
+++ src/js/controllers/users/users-edit.controller.js
@@ -7,16 +7,27 @@ function usersEditCtrl(User, $stateParams, $state) {
   const vm = this;
   vm.user = User.get($stateParams);
 
-  vm.submitForm = update;
-console.log('working1');
-  function update(user) {
-    console.log('working2');
+  vm.submit = user => {
     User
-      .update({ id: user._id }, user)
+      .save(user)
       .$promise
       .then(user => {
         $state.go('usersShow', { id: user._id });
       });
-      console.log('working3');
-  }
+  };
 }
+
+
+//   vm.submitForm = update;
+// console.log('working1');
+//   function update(user) {
+//     console.log('working2');
+//     User
+//       .update({ id: user._id }, user)
+//       .$promise
+//       .then(user => {
+//         $state.go('usersShow', { id: user._id });
+//       });
+//       console.log('working3');
+//   }
+// }
