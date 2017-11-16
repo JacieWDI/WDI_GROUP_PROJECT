2250edf97a8dbb6ea2dd88597e8f8975d48925ae
mattyates52 <matt.yates52@gmail.com>
2017-11-16 11:05:15 +0000

    added some style changes

--- src/js/directives/googlePlace.js
+++ src/js/directives/googlePlace.js
@@ -14,9 +14,7 @@ function googleAutocomplete($window, $rootScope) {
       const inputAutocomplete = new $window.google.maps.places.Autocomplete(element[0], options);
 
       inputAutocomplete.addListener('place_changed', getPlaceData);
-      // // $rootScope.$broadcast('the place has changed,', {
-      //   data: getPlaceData.data
-      // });
+
 
       function getPlaceData() {
         const newPlace = inputAutocomplete.getPlace();
